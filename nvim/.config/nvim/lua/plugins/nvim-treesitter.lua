ad15329d5458e851ef63219b35809be3b0d990dc
Me <TotallyNotLost@proton.me>
2025-02-22 22:06:10 +0000

    feat(nvim): Add c_sharp to treesitter deps

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
-			ensure_installed = { "lua", "vim", "vimdoc", "rust", "kotlin", "python", "javascript" },
+			ensure_installed = { "lua", "vim", "vimdoc", "rust", "kotlin", "python", "javascript", "c_sharp" },
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },  
