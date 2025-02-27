29d9fb84628ad5fcdb30d662623907d090e30034
Me <TotallyNotLost@proton.me>
2025-02-27 10:38:49 -0500

    feat(bash): Add useful aliases

--- bash/.config/bash/aliases.sh
+++ bash/.config/bash/aliases.sh
@@ -1,1 +1,19 @@
+# General CLI
+alias l='lsd'
+alias ll='l -l'
+alias la='ll -a'
+
+# Git aliases
+alias ga='git add'
+alias gc='git commit'
+alias gd='git diff'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+alias gs='git status'
+
+#####################
+# Language-specific #
+#####################
+
+# Golang
 alias gowc='gow -g ~/.config/bash/utils/grcgo.sh'
+
