8656f1b88c564109f4f75ead712df22e9f7942f7
Me <TotallyNotLost@proton.me>
2025-02-27 22:09:28 -0500

    feat(nvim): Update Focusify to use default colorscheme

--- nvim/.config/nvim/lua/plugins/goyo.lua
+++ nvim/.config/nvim/lua/plugins/goyo.lua
@@ -2,9 +2,9 @@ return {
 	"junegunn/goyo.vim",
 	config = function()
 		vim.api.nvim_create_user_command('Focusify', function()
-			vim.cmd("colorscheme slate")
 			vim.cmd("Goyo 72")
 			vim.cmd("Limelight")
+			vim.cmd("colorscheme default")
 		end, {})
 	end,
 }
