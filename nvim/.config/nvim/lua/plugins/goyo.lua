378a948c2e860d67b984ac5d94b807884c5d5ff1
Me <TotallyNotLost@proton.me>
2025-02-28 01:45:49 +0000

    Reconfigure Goyo to add a Focusify command

--- nvim/.config/nvim/lua/plugins/goyo.lua
+++ nvim/.config/nvim/lua/plugins/goyo.lua
@@ -1,3 +1,10 @@
 return {
 	"junegunn/goyo.vim",
+	config = function()
+		vim.api.nvim_create_user_command('Focusify', function()
+		vim.cmd('Goyo 60')
+		vim.cmd('colorscheme slate')
+		vim.cmd('Limelight')
+		end,{})
+	end
 }
