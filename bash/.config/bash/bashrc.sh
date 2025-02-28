67c18181bac0b3083fdc0969a932560f8cfd4f09
Me <TotallyNotLost@proton.me>
2025-02-28 08:44:03 -0500

    feat(bash): Add option to load local bash config

--- bash/.config/bash/bashrc.sh
+++ bash/.config/bash/bashrc.sh
@@ -5,3 +5,8 @@ eval "$(starship init bash)"
 
 # Init FZF
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+if [ -f ~/.config/bash/bashrc-local.sh ]; then
+    source ~/.config/bash/bashrc-local.sh
+fi
+
