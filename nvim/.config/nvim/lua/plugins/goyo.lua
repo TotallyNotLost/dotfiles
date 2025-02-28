890b579ab083d620a1984f87d498858349c0c555
Me <TotallyNotLost@proton.me>
2025-02-27 22:07:58 -0500

    feat(nvim): Add Focusify command

--- nvim/.config/nvim/lua/plugins/goyo.lua
+++ nvim/.config/nvim/lua/plugins/goyo.lua
@@ -1,3 +1,10 @@
 return {
 	"junegunn/goyo.vim",
+	config = function()
+		vim.api.nvim_create_user_command('Focusify', function()
+			vim.cmd("colorscheme slate")
+			vim.cmd("Goyo 72")
+			vim.cmd("Limelight")
+		end, {})
+	end,
 }
