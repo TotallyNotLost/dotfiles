e2226e66d9eb25b0b76e7bd5b030f12a7882b00f
Me <TotallyNotLost@proton.me>
2025-03-04 23:36:08 -0500

    feat(docker): Clone git repo in dw command

--- bash/.config/bash/utils/start-docker-workspace.sh
+++ bash/.config/bash/utils/start-docker-workspace.sh
@@ -2,12 +2,20 @@ NAME=$(gum input --placeholder name)
 TYPE=$(gum choose coding-golang writing)
 
 if [ $TYPE = "coding-golang" ]; then
+	GIT_REPO=$(cat ~/.config/mdnotes/git-repos.txt | sort --reverse | gum filter)
+
+	if [ "$GIT_REPO" = "create new" ]; then
+		GIT_REPO=$(gum input --placeholder "repo")
+		echo $GIT_REPO >> ~/.config/mdnotes/git-repos.txt
+	fi
+
 	sudo docker build \
 		-t $NAME \
 		--build-arg "IMAGE=golang" \
 		--build-arg "LSP_SERVER=gopls" \
 		--build-arg "TEAMOCIL_LAYOUT=golang" \
 		--build-arg "TYPE=$TYPE" \
+		--build-arg "GIT_REPO=$GIT_REPO" \
 		~/.code/github.com/TotallyNotLost/dotfiles/docker/dev-env
 elif [ $TYPE = "writing" ]; then
 	sudo docker build \
