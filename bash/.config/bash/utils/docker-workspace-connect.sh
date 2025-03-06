18b3771d003789960cec98c227f499a737f1b482
Me <TotallyNotLost@proton.me>
2025-03-06 09:10:04 -0500

    fix(docker): Update docker-workspace-connect command to handle cancellation or invalid inputs

--- bash/.config/bash/utils/docker-workspace-connect.sh
+++ bash/.config/bash/utils/docker-workspace-connect.sh
@@ -4,4 +4,6 @@ if [ "$NAME" = "" ]; then
 	NAME=$(cat ~/.config/dw/workspaces.txt | sort --reverse | gum filter)
 fi
 
-sudo docker exec -it $NAME tmux a
+if [ "$NAME" != "" ]; then
+	sudo docker exec -it $NAME tmux a
+fi
