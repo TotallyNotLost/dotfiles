809cc4901b0f853ff2461b2527be6867893e5373
Me <TotallyNotLost@proton.me>
2025-03-06 09:36:15 -0500

    feat(docker): Sort most-recent workspace to top of options

--- bash/.config/bash/utils/docker-workspace-connect.sh
+++ bash/.config/bash/utils/docker-workspace-connect.sh
@@ -6,4 +6,9 @@ if [ "$NAME" = "" ]; then
 	NAME=$(tac ~/.config/dw/workspaces.txt | gum filter)
 fi
 
+# Move the workspace to the bottom of the file so it shows up
+# at the top of the gum filter next time
+sed -i /$NAME/d ~/.config/dw/workspaces.txt
+echo $NAME >> ~/.config/dw/workspaces.txt
+
 sudo docker exec -it $NAME tmux a
