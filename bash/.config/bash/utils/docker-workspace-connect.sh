e75168117066b4f0f8d2625d2dc995d2176f860e
Me <TotallyNotLost@proton.me>
2025-03-30 08:51:47 -0400

    fix(bash): Update docker-workspace-connect to match full workspace name

--- bash/.config/bash/utils/docker-workspace-connect.sh
+++ bash/.config/bash/utils/docker-workspace-connect.sh
@@ -8,7 +8,7 @@ fi
 
 # Move the workspace to the bottom of the file so it shows up
 # at the top of the gum filter next time
-sed -i /$NAME/d ~/.config/dw/workspaces.txt
+sed -i /^$NAME$/d ~/.config/dw/workspaces.txt
 echo $NAME >> ~/.config/dw/workspaces.txt
 
 sudo docker exec -it $NAME tmux a
