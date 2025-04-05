f54002b5fd3e82eb220c4d84f2000e7ba2d5361b
Me <TotallyNotLost@proton.me>
2025-04-05 10:13:19 -0400

    feat(bash): Add gco alias

--- bash/.config/bash/aliases.sh
+++ bash/.config/bash/aliases.sh
@@ -9,6 +9,7 @@ alias v='nvim'
 # Git
 alias ga='git add'
 alias gc='git commit'
+alias gco='git checkout'
 alias gd='git diff'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 alias gp='git pull'
