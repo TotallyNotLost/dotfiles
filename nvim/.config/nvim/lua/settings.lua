a7d4c237a99d63b1c7953d267ee17d8dd2f53496
Me <TotallyNotLost@proton.me>
2025-04-05 10:10:34 -0400

    feat(nvim): Add auto-session plugin

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -6,11 +6,3 @@ local api = vim.api
 
 api.nvim_set_keymap("n", "<leader>-", ":split<CR>", {})
 api.nvim_set_keymap("n", "<leader>|", ":vsplit<CR>", {})
-
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    if vim.fn.argv(0) == "" then
-      require("telescope.builtin").find_files()
-    end
-  end,
-})
