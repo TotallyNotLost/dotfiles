b0db300f67e15e0f42dc799a105dc4b3c990fbdd
Me <TotallyNotLost@proton.me>
2025-04-05 10:02:59 -0400

    feat(bash): Add aliases

--- bash/.config/bash/aliases.sh
+++ bash/.config/bash/aliases.sh
@@ -3,12 +3,18 @@ alias l='lsd --date relative'
 alias ll='l -l'
 alias la='ll -a'
 
+# Nvim
+alias v='nvim'
+
 # Git
 alias ga='git add'
 alias gc='git commit'
 alias gd='git diff'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+alias gpsh='git push'
 alias gs='git status'
+alias gsh='git stash'
+alias gshp='git stash pop'
 
 # Docker
 alias dw='~/.config/bash/utils/docker-workspace-connect.sh'
@@ -24,11 +30,3 @@ alias gowc='gow -g ~/.config/bash/utils/grcgo.sh'
 
 # Writing
 alias new-outline='cat ~/.code/github.com/TotallyNotLost/dotfiles/docker/dev-env/writing/outline.md >> outline.md'
-
-
-####################
-# Custom functions #
-####################
-notes() {
-	grep -C 1 -ri $1 $2 | fold -w 80
-}
