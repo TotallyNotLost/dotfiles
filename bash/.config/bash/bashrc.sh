023b672eafa1938bd88a8c859e6a4bbabe4fd776
Me <TotallyNotLost@proton.me>
2025-04-12 09:33:05 -0400

    feat(bash): Support /usr/bin and /usr/local/go/bin install locations

--- bash/.config/bash/bashrc.sh
+++ bash/.config/bash/bashrc.sh
@@ -12,7 +12,11 @@ if [ -f ~/.config/bash/bashrc-local.sh ]; then
     source ~/.config/bash/bashrc-local.sh
 fi
 
+# Golang
+if [ -f /usr/local/go/bin/go ]; then
+    PATH=$PATH:/usr/local/go/bin
+fi
 if command -v go 2>&1 >/dev/null; then
-    PATH=$PATH:/usr/local/go/bin:$HOME/.go/bin
     go env -w GOPATH=$HOME/.go
+    PATH=$PATH:$HOME/.go/bin
 fi
