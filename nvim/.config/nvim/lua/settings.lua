e1ff11394ee3d9bee9899d445ddcf8086e0ec3c5
Me <TotallyNotLost@proton.me>
2025-07-25 13:45:47 -0400

    feat: Updates

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -5,5 +5,19 @@ vim.cmd("set number relativenumber")
 
 local api = vim.api
 
+-- Window management
 api.nvim_set_keymap("n", "<leader>-", ":split<CR>", {})
 api.nvim_set_keymap("n", "<leader>|", ":vsplit<CR>", {})
+
+-- Helpers
+api.nvim_set_keymap("n", "<A-s>", ":NvimTreeFindFileToggle<CR>:Trouble diagnostics toggle<CR>:Trouble symbols toggle<CR>", {})
+
+--
+-- Neovide
+--
+if vim.g.neovide then
+    vim.o.guifont = "monospace:h10"
+    -- For whatever reason, this looks bad on CLI
+    vim.cmd("colorscheme tokyonight")
+end
+
