e7a0ea1ee05ec13a8ec463f6e0f23f117f4c13fb
Me <TotallyNotLost@proton.me>
2025-07-29 20:57:39 +0000

    feat(television): Replace fzf with television

--- bash/.config/bash/bashrc.sh
+++ bash/.config/bash/bashrc.sh
@@ -4,8 +4,8 @@ export EDITOR=nvim
 # Starship
 eval "$(starship init bash)"
 
-# FZF
-[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+# Television
+echo 'eval "$(tv init bash)"' >> ~/.bashrc
 
 # Local config
 if [ -f ~/.config/bash/bashrc-local.sh ]; then
